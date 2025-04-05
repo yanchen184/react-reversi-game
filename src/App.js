8078714d6d0c18bdda35e4cc4300f7d35e8ead07
yanchen184 <62279165+yanchen184@users.noreply.github.com>
2025-04-05 17:17:52 +0800

    Update App.js to read version from package.json

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import Game from './components/Game';
+import packageInfo from '../package.json';
 
-// 遊戲版本
-const GAME_VERSION = 'v1.0.0';
+// Get version from package.json
+const GAME_VERSION = `v${packageInfo.version}`;
 
 function App() {
   return (
